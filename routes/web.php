a459f44d0c6a9bd36cfc778eac1b371b7bc64f4a
1999-keiji <keiji.engine@gmail.com>
2022-10-27 19:31:47 +0900

    user change password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Backend\UserController;
 use Illuminate\Support\Facades\Route;
+use Backend\UserController;
+use Backend\ChangePasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', UserController::class);
+// クラスを指定する場合は[]で定義コントローラを括る
+Route::post('users/{user}/change-password',
+ [App\Http\Controllers\Backend\ChangePasswordController::class, 'change_password'])
+->name('users.change.password');
